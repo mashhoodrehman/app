dbdb795a18cb194625f1299c889a8fd951aa2dbc
mashhoodrehman <mashhoodr.rehman@gmail.com>
2019-01-18 10:23:53 +0500

    tester

--- index.php
+++ index.php
@@ -54,8 +54,7 @@
  * NOTE: If you change these, also change the error_reporting() code below
  */
 	define('ENVIRONMENT', isset($_SERVER['CI_ENV']) ? $_SERVER['CI_ENV'] : 'development');
-	echo ENVIRONMENT;
-	exit();
+
 
 /*
  *---------------------------------------------------------------
@@ -68,8 +67,9 @@
 switch (ENVIRONMENT)
 {
 	case 'development':
-		error_reporting(-1);
 		ini_set('display_errors', 1);
+		ini_set('display_startup_errors', 1);
+		error_reporting(E_ALL);
 	break;
 
 	case 'testing':
